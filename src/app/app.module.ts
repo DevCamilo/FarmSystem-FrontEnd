9f2a1eee5af00938e0cdd197d5c4daff5e40f204
Camilo <develop.reyes@gmail.com>
2018-12-30 23:42:48 -0600

    :zap: Add Routes :zap:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
+// Importaciones
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+
+// Rutas
+import { app_routing } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
